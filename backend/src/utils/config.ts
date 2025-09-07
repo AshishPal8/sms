e02af6c33137104636f17c16fc7675e1320c0e96
Ashish Pal <ashish81301669@gmail.com>
2025-09-07 20:43:39 +0530

    update image kit to cloudinary

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -4,6 +4,7 @@ dotenv.config();
 export const port = process.env.PORT;
 export const jwtSecret = process.env.JWT_SECRET!;
 export const backendUrl = process.env.BACKEND_URL!;
-export const imagekitPublicKey = process.env.IMAGEKIT_PUBLIC_KEY!;
-export const imagekitPrivateKey = process.env.IMAGEKIT_PRIVATE_KEY!;
-export const imagekitUrlEndpoint = process.env.IMAGEKIT_URL_ENDPOINT!;
+
+export const cloudinaryCloudName = process.env.CLOUDINARY_CLOUD_NAME!;
+export const cloudinaryApiKey = process.env.CLOUDINARY_API_KEY!;
+export const cloudinaryApiSecret = process.env.CLOUDINARY_API_SECRET!;
